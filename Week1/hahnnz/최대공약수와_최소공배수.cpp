5f48040d98381e039c2b1049be5d609993d2f2d7
Hahnnz <lphion96@naver.com>
2018-04-01 22:18:28 +0900

    added just for test

---
+++ Week1/hahnnz/최대공약수와_최소공배수.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+vector<int> gcdlcm(int a,int b)
+{
+    vector<int> answer;
+    answer.reserve(2); 
+    
+    int i,j;
+    int gcd = 0;
+    int lcm = 0;
+
+    j = (a<b)?a:b;
+    for(i=1;i<=j;i++){
+        if (a%i==0 && b%i==0) gcd=i;
+    }
+    j = (a>b)?a:b;
+    for(i=j;;i++){
+        if (i%a==0 && i%b==0){
+            lcm=i;
+            break;
+        }
+    }
+
+    answer.push_back(gcd);
+    answer.push_back(lcm);
+
+	return answer;
+}
+
+int main()
+{
+    int a=3, b=12;
+    vector<int> testAnswer = gcdlcm(a,b);
+
+    cout<<testAnswer[0]<<" "<<testAnswer[1];
+}
