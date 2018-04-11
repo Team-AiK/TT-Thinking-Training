f83ead146975d073ba9ddf4590dc2bd93dda3a08
groovypark <soobin950@gmail.com>
2018-04-11 21:05:07 +0900

    2016년

--- Week2/groovypark/5_getDayName.py
+++ Week2/groovypark/5_getDayName.py
@@ -6,6 +6,12 @@ def getDayName(a,b):
         sumDay = sumDay + day
     answer = dayName[(sumDay+b)%7]
 
+    # 다른사람코드
+    # def getDayName(a,b):
+    # months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    # return days[(sum(months[:a-1])+b-1)%7]
+    
     return answer
 
 #아래 코드는 테스트를 위한 출력 코드입니다.
