f39ea894aa2b787d4f041e443d1980d66ec68265
groovypark <soobin950@gmail.com>
2018-05-01 17:04:01 +0900

    2018 Summer Coding 1차 - 테스트 문제 1

--- Week3/groovypark/3_longest_palindrom.py
+++ Week3/groovypark/3_longest_palindrom.py
@@ -7,9 +7,9 @@ def longest_palindrom(s):
     result = []
     for i in range(len(s)):
         for j in range(1,len(s)+1):
-            if s[i:j] and str(s[i:j]) == str(s[i:j])[::-1]:
+            if s[i:j] == s[i:j][::-1]:
                 result.append(len(s[i:j]))
-            if s[j:i] and str(s[j:i]) == str(s[j:i])[::-1]:
+            if s[j:i] == s[j:i][::-1]:
                 result.append(len(s[j:i]))
     return max(result)
 
